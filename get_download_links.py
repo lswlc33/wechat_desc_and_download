71afcf5309e8d5fcb5d44205b1daf67f5ad78ad3
lswlc33 <lswlc33@qq.com>
2023-07-08 20:18:55 +0800

    精简代码

--- get_download_links.py
+++ get_download_links.py
@@ -1,10 +1,10 @@
 # 暴力获取微信7~8大版本的最新小版本
-# 耗时比较长,
-# 结果最后将在终端呈现
+# 耗时比较长,可能要半小时
+# 结果最后将在终端呈现（划掉）
+# 最后结果将在download_links.py呈现
 
-import requests, re
 from datetime import datetime
-
+from check_and_up import *
 
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -27,45 +27,19 @@ result_list = {}
 
 def get(v1, v2):
     the_url = url.format(v1, v2)
-    response = requests.head(url=the_url, headers=headers)
-    result = str(response.status_code)
+    result = check_error_link(the_url)
     c_time = datetime.now().strftime("[%H:%M:%S]")
 
-    if response.status_code == 200:
+    if result:
         the_url = check_update(the_url)
-
         global result_list
         result_list[v1] = the_url
         print("succeed")
-        print(c_time + " " + result + " " + the_url)
+        print(c_time + " " + str(result) + " " + the_url)
         return 1
     else:
-        print(c_time + " " + result + " " + the_url)
+        print(c_time + " " + str(result) + " " + the_url)
         return 0
-
-
-def check_update(the_url):
-    r_code = 200
-    num = 1
-    c_url = the_url
-    # 制作新链接
-    if re.search(r'_\d+\.apk$', the_url):
-        num = int(re.search(r'(\d+)\.apk$', the_url).group(1)) + 1
-        url1 = the_url[:-6] + "_{}.apk"
-    else:
-        url1 = the_url[:-4] + "_{}.apk"
-
-    # 测试链接
-    while r_code == 200:
-        new_url = url1.format(num)
-        r_code = requests.head(url=new_url, headers=headers).status_code
-        if r_code == 200:
-            num += 1
-            c_url = new_url
-        else:
-            break
-
-    return c_url
 
 
 def run():
@@ -78,6 +52,7 @@ def run():
                     c_ver_code = j
                     pass
 
+    # 这里是查找范围
     run_get_link(700, 710, 1380)
     run_get_link(7010, 7023, 1580)
     run_get_link(800, 810, 1840)
